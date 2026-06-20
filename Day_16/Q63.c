df0de681acecb50539dd6a27142170976f280092
Rahul <bindrahul1818@gmail.com>
2026-06-20 07:47:29 +0530

    Sixteen commit

---
+++ Day_16/Q63.c
@@ -0,0 +1,29 @@
+//WAP to find pair with given sum
+#include <stdio.h>
+int main()
+{
+    int n, i, j, sum;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the required sum: ");
+    scanf("%d", &sum);
+
+    printf("Pairs with sum %d are:\n", sum);
+    for(i = 0; i < n; i++)
+    {
+        for(j = i + 1; j < n; j++)
+        {
+            if(arr[i] + arr[j] == sum)
+            {
+                printf("(%d, %d)\n", arr[i], arr[j]);
+            }
+        }
+    }
+    return 0;
+}
